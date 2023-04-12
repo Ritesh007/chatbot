7e29c46f052bff8638395ad6ab84b598865dc48c
ritesh007 <rithu.ritesh@gmail.com>
2023-04-11 23:29:57 -0500

    openai integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 function App() {
   const [messages, setMessages] = useState([
@@ -17,11 +23,16 @@ function App() {
     }
   };
 
-  const sendMessage = () => {
+  const sendMessage = async () => {
     const userMessage = { text: inputValue, isBot: false };
     const newMessage = { text: '...', isBot: true };
     setMessages((prevMessages) => [...prevMessages, userMessage, newMessage]);
     setInputValue('');
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: inputValue,
+    });
+    console.log(completion);
     fetch('https://example.com/chatbot', {
       method: 'POST',
       headers: {
